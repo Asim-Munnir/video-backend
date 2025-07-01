35bdaf021e6c58e1ccb19c58ece097a3c4be02a3
Asim-Munnir <asimmunnir1@gmail.com>
2025-07-01 12:40:28 +0500

    add register controller and bug fixes

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -4,9 +4,11 @@ class ApiResponse {
         data,
         message = "Success"
     ){
-        this.statusCode,
+        this.statusCode = statusCode
         this.data = data
         this.message = message
         this.success = statusCode < 400
     }
 }
+
+export { ApiResponse }
