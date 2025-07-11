761f3f6faa9b84d66fd78d494b5adbacbab24dc5
Asim-Munnir <asimmunnir1@gmail.com>
2025-07-12 01:28:47 +0500

    auth middleware and login and logout functionallity and access and clear tokens and verify user through token

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { veriftJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
@@ -23,6 +24,10 @@ router.route("/register").post(
     registerUser
 )
 
+router.route("/login").post(loginUser)
 
+
+// secured routes
+router.route("/logout").post(veriftJWT, logoutUser)
 
 export default router
